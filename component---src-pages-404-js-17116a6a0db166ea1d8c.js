9be6bd4bb0449e1c5b54941a88b65fb25f2ef628
rafosorio <rafosorio@gmail.com>
2024-01-15 20:06:11 -0300

    Updates

---
+++ component---src-pages-404-js-17116a6a0db166ea1d8c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[18],{w2l6:function(e,n,t){"use strict";t.r(n);t("q1tI");var o=t("Wjw8"),s=t("qKvR"),r=[{href:"/components/markdown",text:"Markdown"},{href:"/components/Aside",text:"Aside"},{href:"/components/demo",text:"Demo"}];n.default=function(){return Object(s.b)(o.a,{links:r})}}}]);
+//# sourceMappingURL=component---src-pages-404-js-17116a6a0db166ea1d8c.js.map
