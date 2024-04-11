d22bd492aa46b09cce0d8a069e8394a8e0a73946
rafosorio <rafosorio@gmail.com>
2024-04-11 16:22:47 -0300

    ok

--- src/gatsby-theme-carbon/components/Header.js
+++ src/gatsby-theme-carbon/components/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Header from 'gatsby-theme-carbon/src/components/Header';
 
+import { InlineNotification } from 'gatsby-theme-carbon/src/components/inline-notification';
+
 const CustomHeader = props => (
   <Header {...props}>
     <span>Integration &nbsp;</span> Learning Hub
